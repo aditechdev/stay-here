dd7dbbe23ca0a4b8442a99d58554d50777155454
dev adi <aditya.anand156@gmail.com>
2018-05-14 13:28:48 +0530

    --main activity

--- app/src/main/java/com/example/android/stayhere/MainActivity.java
+++ app/src/main/java/com/example/android/stayhere/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.android.stayhere;
 
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpToolbar();
+        navigationView= (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this, "Clicked Home", Toast.LENGTH_SHORT);
+                        break;
+                    case R.id.jja:
+                        Toast.makeText(MainActivity.this, "Clicked Just Java Apps", Toast.LENGTH_SHORT);
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
+    private void setUpToolbar(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+
 }
